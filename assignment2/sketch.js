d099a4887718b0581bf0dad2c2c425cc70b83390
Dustin Wleczyk <dustinwleczyk@hotmail.com>
2024-02-05 19:44:27 -0600

    Finished

--- assignment2/sketch.js
+++ assignment2/sketch.js
@@ -1,6 +1,4 @@
-
-
-let currentlySelectedColor = ""
+let currentlySelectedColor = "black"; 
 
 class StyledButton {
   constructor(text, x, y, bgColor, padding = '25px') {
@@ -8,76 +6,53 @@ class StyledButton {
     this.button.position(x, y);
     this.button.style('background-color', bgColor);
     this.button.style('padding', padding);
-  }
-
-  onClick(callback) {
-    this.button.mousePressed(callback);
+    this.button.mousePressed(() => currentlySelectedColor = bgColor);
   }
 }
-
 
 function setup() {
   createCanvas(1000, 800);
+  background(220);
+  noStroke();
+
+  new StyledButton("", 0, 0, "red");
+  new StyledButton("", 0, 54, "orange");
+  new StyledButton("", 0, 108, "yellow");
+  new StyledButton("", 0, 162, "#7FFF00"); // Light green
+  new StyledButton("", 0, 216, "#00FFFF"); // Cyan
+  new StyledButton("", 0, 270, "#0000FF"); // Blue
+  new StyledButton("", 0, 324, "#FF00FF"); // Magenta
+  new StyledButton("", 0, 378, "#8B4513"); // Brown
+  new StyledButton("", 0, 434, "white");
+  new StyledButton("", 0, 488, "black");
 }
 
 function draw() {
-  background(255);
+  if (mouseIsPressed && mouseX > 54) {
+    stroke(currentlySelectedColor);
+    strokeWeight(10);
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+}
 
-  let redButton = new StyledButton("", 0, 0, "red")
-  redButton.onClick(() => {
-    currentlySelectedColor = 'red';
-  });
+function drawPalette() {
+  noStroke();
+  const colors = ["red", "orange", "yellow", "#7FFF00", "#00FFFF", "#0000FF", "#FF00FF", "#8B4513", "white", "black"];
+  for (let i = 0; i < colors.length; i++) {
+    fill(colors[i]);
+    rect(0, i * 54, 54, 54); 
+  }
+}
 
-  let orangeButton = new StyledButton("", 0, 54, "orange")
-  orangeButton.onClick(() => {
-    currentlySelectedColor = 'orange';
-  });
+function mousePressed() {
+  if (mouseX <= 54) {
+    const index = Math.floor(mouseY / 54);
+    if (index >= 0 && index < 10) {
+      currentlySelectedColor = ["red", "orange", "yellow", "#7FFF00", "#00FFFF", "#0000FF", "#FF00FF", "#8B4513", "white", "black"][index];
+    }
+  }
+}
 
-  let yellowButton = new StyledButton("", 0, 108, "yellow")
-  yellowButton.onClick(() => {
-    currentlySelectedColor = 'yellow';
-  });
-
-  let lightGreenButton = new StyledButton("", 0, 162, "#7FFF00")
-  lightGreenButton.onClick(() => {
-    currentlySelectedColor = '#7FFF00';
-  });
-
-  let lightBlueButton = new StyledButton("", 0, 216, "#00FFFF")
-  lightBlueButton.onClick(() => {
-    currentlySelectedColor = '#00FFFF';
-  });
-
-  let blueButton = new StyledButton("", 0, 270, "#0000FF")
-  blueButton.onClick(() => {
-    currentlySelectedColor = '#0000FF';
-  });
-
-
-  let hotPinkButton = new StyledButton("", 0, 324, "#FF00FF")
-  hotPinkButton.onClick(() => {
-    currentlySelectedColor = '#FF00FF';
-  });
-
-
-  let brownButton = new StyledButton("", 0, 378, "#8B4513")
-  brownButton.onClick(() => {
-    currentlySelectedColor = '#8B4513';
-  });
- 
-
-  let whiteButton = new StyledButton("", 0, 432, "white")
-  whiteButton.onClick(() => {
-    currentlySelectedColor = 'white';
-  });
-
-
-  let blackButton = new StyledButton("", 0, 486, "black")
-  blackButton.onClick(() => {
-    currentlySelectedColor = 'black';
-  });
-
-
-
-
+function mouseReleased() {
+  drawPalette(); 
 }
