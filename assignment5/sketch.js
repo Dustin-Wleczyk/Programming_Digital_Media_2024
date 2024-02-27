28bfb13d0f38a344f01390ae8f02c0a175a393ed
Dustin Wleczyk <dustinwleczyk@hotmail.com>
2024-02-27 15:03:29 -0600

    Added the code

--- assignment5/sketch.js
+++ assignment5/sketch.js
@@ -1,36 +1,65 @@
+let sounds = new Tone.Players({
+  'chicken': "assets/chicken.mp3",
+  'explosion': "assets/explosion.mp3",
+  'firework': "assets/firework.mp3",
+  'glass': "assets/glass.mp3"
+   });
+ 
+let delAmt = new Tone.FeedbackDelay ("8n", 0.5);
+let distAmt = new Tone.Distortion (0.5); 
 
-// function preload() {
-//   soundFx = new Tone.Players({
-//     popcorn: "assets/popcorn.mp3",
-//     water: "assets/water.mp3"
-//   }).toDestination();
-// }
+let button1;
+let button2;
 
-function preload() {
-  soundFx = new Tone.Players({
-    water: "assets/water.mp3"
-  }).toDestination();
-}
+let delaySlider;
+let fbSlider;
+let distSlider;
 
-function keyPressed() {
-  if (key == "q") {
-    soundFx.player('water').start()
-  }
-}
 
-function play1() {
-  soundFx.player('water').start()
-}
+
+sounds.connect(delAmt);
+delAmt.connect(distAmt);
+distAmt.toDestination();
+
 
 function setup() {
-
-  button1 = createButton("water")
-  button1.position(50,50)
-  button1.mousePressed(play1())
-
   createCanvas(400, 400);
+
+  button1 = createButton("Chicken");
+  button1.position(50,50);
+  button1.mousePressed(() => sounds.player("chicken").start());
+
+  button2 = createButton("Explosion");
+  button2.position(120,50);
+  button2.mousePressed(() => sounds.player("explosion").start());
+
+  button3 = createButton("Firework");
+  button3.position(198,50);
+  button3.mousePressed(() => sounds.player("firework").start());
+
+  button4 = createButton("Glass");
+  button4.position(275,50);
+  button4.mousePressed(() => sounds.player("glass").start());
+
+  delaySlider = createSlider(0, 1, 0, 0.05);
+  delaySlider.position(width/4, 110);
+  delaySlider.mouseMoved(() => delAmt.delayTime.value = delaySlider.value());
+
+  fbSlider = createSlider(0, 0.9, 0, 0.05);
+  fbSlider.position(width/4, 150);
+  fbSlider.mouseMoved(() => delAmt.feedback.value = fbSlider.value());
+
+  distSlider = createSlider(0, 0.9, 0, 0.05);
+  distSlider.position(width/4, 190);
+  distSlider.mouseMoved( () => distAmt.distortion = distSlider.value());
+
 }
 
 function draw() {
-  background(220);
+  background(10, 150, 255);
+
+  text ("Press buttons for sound", width/3, 30);
+  text ("Add delay", width/3, 105);
+  text ("Add feedback", width/3, 145);
+  text ("Add distortion", width/3, 185);
 }
